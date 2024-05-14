b74c8a5df02b9cc67c5c26f91a6132e82f110bb9
unknown <apid9755@gmail.com>
2024-05-14 22:02:19 +0800

    release tonton

---
+++ services/tonton.py
@@ -0,0 +1,18 @@
+def get_headers():
+    headers = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36",
+    "Origin": "https://watch.tonton.com.my",
+    "Sec-Fetch-Site": "cross-site",
+    "Sec-Fetch-Mode": "cors",
+    "Sec-Fetch-Dest": "empty",
+    "Referer": "https://watch.tonton.com.my/"
+    }
+    return headers
+
+def get_params():
+    params = {'contentId': 'HERE','ls_session': 'HERE'}
+    return params
+
+def get_data():
+    data = "HERE"
+    return data

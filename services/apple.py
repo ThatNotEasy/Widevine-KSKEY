031474a1527e0480c207dc36ed22906ef1face70
unknown <apid9755@gmail.com>
2024-05-16 06:19:51 +0800

    release apple & bitmovin (test)

---
+++ services/apple.py
@@ -0,0 +1,7 @@
+def get_headers():
+    headers = {}
+    return headers
+
+def get_data():
+    data = {}
+    return data
---
+++ services/bitmovin.py
@@ -0,0 +1,29 @@
+def get_cookies():
+    cookies = {}
+    return cookies
+
+def get_headers():
+    headers = {
+    'accept': '*/*',
+    'accept-language': 'en-US,en;q=0.9',
+    'origin': 'https://bitmovin.com',
+    'priority': 'u=1, i',
+    'referer': 'https://bitmovin.com/',
+    'sec-ch-ua': '"Chromium";v="124", "Google Chrome";v="124", "Not-A.Brand";v="99"',
+    'sec-ch-ua-mobile': '?0',
+    'sec-ch-ua-platform': '"Windows"',
+    'sec-fetch-dest': 'empty',
+    'sec-fetch-mode': 'cors',
+    'sec-fetch-site': 'cross-site',
+    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36',
+    'content-type': 'application/x-www-form-urlencoded',
+    }
+    return headers
+
+def get_params():
+    params = {}
+    return params
+
+def get_data():
+    data = ''
+    return data

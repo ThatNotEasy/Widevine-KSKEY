03e917ae8037295314ae7ec77b746c4164217a8b
unknown <apid9755@gmail.com>
2024-05-16 06:26:56 +0800

    release apple, hbogo & bitmovin (test)

--- main.py
+++ main.py
@@ -5,16 +5,30 @@ import requests
 import importlib
 import pyfiglet
 from base64 import b64encode
-from colorama import init, Fore
+from colorama import init
 from modules.deviceconfig import device_android_generic
 from modules.pssh import get_pssh
 from modules.wvdecryptcustom import WvDecrypt
+from services.hbogo import get_license
+from loguru import logger
 import coloredlogs
-from test import json_data
 
 # Initialize colorama and coloredlogs
 init(autoreset=True)
 coloredlogs.install(level='DEBUG')
+
+# Configure loguru
+logger.remove()
+logger.add(sys.stdout, colorize=True, format="<green>{time}</green> <level>{message}</level>", level="DEBUG")
+
+def parse_arguments():
+    parser = argparse.ArgumentParser(description='WKS-KEYS 2.0 - A tool to obtain Widevine keys from MPD URLs')
+    parser.add_argument('-u', '--license-url', required=False, help='URL to request Widevine license')
+    parser.add_argument('-m', '--mpd-url', help='URL of the Media Presentation Description (MPD)')
+    parser.add_argument('-p', '--pssh', required=False, help='Protection System Specific Header (PSSH)')
+    parser.add_argument('-s', '--service', required=True, help='Specify the service module (e.g., prime, netflix)')
+    parser.add_argument('-c', '--content-id', required=False, help='Specify the content id for HBOGO modules')
+    return parser.parse_args()
 
 def generate_pssh(mpd_url):
     pssh = get_pssh(mpd_url)
@@ -22,76 +36,101 @@ def generate_pssh(mpd_url):
 
 def print_title(title_text):
     title = pyfiglet.figlet_format(title_text, font='slant')
-    print(Fore.MAGENTA + title + Fore.RESET)
+    logger.debug(title)
 
 def get_service_module(service_name):
     try:
         return importlib.import_module(f'services.{service_name}')
     except ImportError:
-        print(f"No module named '{service_name}' found in 'services' package")
+        logger.error(f"No module named '{service_name}' found in 'services' package")
         sys.exit(1)
 
-def get_license_keys(pssh, lic_url, service_module):
-    service = get_service_module(service_module)
-    headers = service.get_headers()
-    data = service.get_data()
+def get_license_keys(pssh, lic_url, service_module, content_id=None):
+    if service_module == "hbogo":
+        if not content_id:
+            logger.error("Content ID is required for HBOGO service.")
+            return False, None
+        data = get_license(content_id)
+        logger.debug(data)  # This line is just for testing purposes. You can remove it.
+        return True, []  # HBOGO license retrieval does not require other steps, so we return an empty list of keys
+    else:
+        service = get_service_module(service_module)
+        headers = service.get_headers()
+        data = service.get_data()
 
-    # Conditional handling of params and cookies based on the existence of these methods in the service module
-    params = getattr(service, 'get_params', lambda: {})()  # Returns {} if get_params is not available
-    cookies = getattr(service, 'get_cookies', lambda: {})()  # Returns {} if get_cookies is not available
+        # Conditional handling of params and cookies based on the existence of these methods in the service module
+        params = getattr(service, 'get_params', lambda: {})()  # Returns {} if get_params is not available
+        cookies = getattr(service, 'get_cookies', lambda: {})()  # Returns {} if get_cookies is not available
 
-    wvdecrypt = WvDecrypt(init_data_b64=pssh, cert_data_b64=None, device=device_android_generic)
-    challenge = wvdecrypt.get_challenge()
+        wvdecrypt = WvDecrypt(init_data_b64=pssh, cert_data_b64=None, device=device_android_generic)
+        challenge = wvdecrypt.get_challenge()
 
-    if service_module == "prime":
-        data['widevine2Challenge'] = b64encode(challenge).decode()
-        response = requests.post(url=lic_url, headers=headers, params=params, cookies=cookies, data=data)
-        license_b64 = response.json()["widevine2License"]["license"]
-        wvdecrypt.update_license(license_b64)
-        Correct, keys = wvdecrypt.start_process()
-        return Correct, keys
+        if not pssh:
+            logger.error("No PSSH data provided or extracted.")
+            return False, None
 
-    elif service_module == "astro":
-        json_data['licenseChallenge'] = b64encode(challenge).decode()
-        response = requests.post(url=lic_url, headers=headers, json=json_data)
-        license_b64 = response.json()["licenseData"][0]
-        wvdecrypt.update_license(license_b64)
-        Correct, keys = wvdecrypt.start_process()
-        return Correct, keys
+        if service_module == "prime":
+            data['widevine2Challenge'] = b64encode(challenge).decode()
+            response = requests.post(url=lic_url, headers=headers, params=params, cookies=cookies, data=data)
+            license_b64 = response.json()["widevine2License"]["license"]
+            wvdecrypt.update_license(license_b64)
+            Correct, keys = wvdecrypt.start_process()
+            return Correct, keys
+
+        elif service_module == "astro":
+            data['licenseChallenge'] = b64encode(challenge).decode()
+            response = requests.post(url=lic_url, headers=headers, json=data)
+            license_b64 = response.json()["licenseData"][0]
+            wvdecrypt.update_license(license_b64)
+            Correct, keys = wvdecrypt.start_process()
+            return Correct, keys
+        
+        elif service_module == "tonton":
+            response = requests.post(url=lic_url, params=params, headers=headers, data=challenge)
+            license_b64 = b64encode(response.content)
+            wvdecrypt.update_license(license_b64)
+            Correct, keys = wvdecrypt.start_process()
+            return Correct, keys
+        
+        elif service_module == "apple":
+            data['streaming-request']['streaming-keys'][0]['challenge'] = b64encode(challenge).decode()
+            pssh = data['streaming-request']['streaming-keys'][0]['uri'].replace('data:text/plain;base64,', '')
+            logger.debug("PSSH Data: {}", pssh)
+            response = requests.post(url=lic_url, headers=headers, json=data)
+            license_b64 = response.json()['streaming-response']['streaming-keys'][0]['license']
+            wvdecrypt.update_license(license_b64)
+            Correct, keys = wvdecrypt.start_process()
+            return Correct, keys
+        
+        elif service_module == "bitmovin":
+            response = requests.post(url=lic_url, headers=headers, data=challenge)
+            license_b64 = b64encode(response.content)
+            wvdecrypt.update_license(license_b64)
+            Correct, keys = wvdecrypt.start_process()
+            return Correct, keys
 
 def print_license_keys(keys):
     for key in keys:
-        print(Fore.CYAN + f'--key {key}' + Fore.RESET)
-    print(Fore.GREEN + "\nAll Done..." + Fore.RESET)
+        logger.info(f'--key {key}')
+    logger.success("\nAll Done...")
 
-def parse_arguments():
-    parser = argparse.ArgumentParser(description='WKS-KEYS 2.0 - A tool to obtain Widevine keys from MPD URLs')
-    parser.add_argument('-u', '--license-url', required=True, help='URL to request Widevine license')
-    parser.add_argument('-m', '--mpd-url', help='URL of the Media Presentation Description (MPD)')
-    parser.add_argument('-p', '--pssh', required=False, help='Protection System Specific Header (PSSH)')
-    parser.add_argument('-s', '--service', required=True, help='Specify the service module (e.g., prime, netflix)')
-    return parser.parse_args()
-
-def clear_screen():
-    os.system('cls' if os.name == 'nt' else 'clear')
 
 def main():
-    clear_screen()
-    print_title('WKS-KEYS 2.0')
+    print_title('\nWidevine-KSKEY')
     args = parse_arguments()
     lic_url = args.license_url
 
     pssh = args.pssh if args.pssh else generate_pssh(args.mpd_url) if args.mpd_url else None
-    
-    if not pssh:
-        print("No PSSH data provided or extracted.")
+    if not pssh and args.service != "hbogo":
+        logger.error("No PSSH data provided or extracted.")
         return
 
-    correct, keys = get_license_keys(pssh, lic_url, args.service)
+    correct, keys = get_license_keys(pssh, lic_url, args.service, args.content_id)
     if correct:
         print_license_keys(keys)
     else:
-        print("Failed to retrieve valid keys.")
+        logger.error("Failed to retrieve valid keys.")
 
 if __name__ == "__main__":
+    os.system('cls' if os.name == 'nt' else 'clear')
     main()
---
+++ services/hbogo.py
@@ -0,0 +1,36 @@
+import requests
+import json
+
+def get_headers():
+    headers = {}
+    return headers
+
+def get_license(content_id):
+    url = f"https://api3.hbogoasia.com/v1/movie?contentId={content_id}&territory=MYS"
+    response = requests.get(url, headers=get_headers())
+    data = response.json()
+    content_id = data.get("contentId")
+    materials = data.get("materials", [])
+    image_href = next((material.get("href") for material in materials if isinstance(material, dict) and material.get("type") == "image"), None)
+    genre = data.get("metadata", {}).get("genre")
+    title_info = next(iter(data.get("metadata", {}).get("titleInformations", [])), {})
+    name = title_info.get("name")
+    summary = title_info.get("summary")
+
+    if content_id:
+                                                                                                        # REPLACE "{TOKEN}" & "{PARTNER_ID}" WITH YOUR 
+        response = requests.get(f"https://api3.hbogoasia.com/v1/asset/playbackurl?territory=MYS&contentId={content_id}&sessionToken={TOKEN}&channelPartnerID={PARTNER_ID}&operatorId=SIN&lang=en", headers=get_headers())
+        if response.status_code == 200:
+            datas = response.json()
+            mpdURL = datas.get("playbackURL")
+            license_urls = datas.get("licenseURLs", {}).get("widevine")
+            if mpdURL and license_urls:
+                cdm = {"license_url": license_urls, "pssh": "AAAAP3Bzc2gAAAAA7e+LqXnWSs6jyCfc1R0h7QAAAB8SEJIhyOZ8dW8JT0VCAkLYVg4aBWV6ZHJtSOPclZsG"}
+                cdm_headers = {'X-API-Key': '58146edc021d601f4c6130d7e64062f8168ecc8092a63633bbdd094d7249cbb1', 'Content-Type': 'application/json', 'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36'}
+                meow = requests.post("https://keysdb.net/api", json=cdm, headers=cdm_headers)
+                meowing = meow.json()
+                kid = meowing.get('kid')
+                key = meowing.get('keys', [{}])[0].get('key', '').split(':')[1]
+                response_data = {"thumbnail": image_href, "title": name, "synopsis": summary, "genre": genre, "kid": kid, "key": key, "mpdURL": mpdURL}
+                return json.dumps(response_data, indent=2)
+    return None

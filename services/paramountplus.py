d9d3bf3fb3340428c01016f595f8eb19c1553f4b
unknown <apid9755@gmail.com>
2024-05-17 07:57:43 +0800

    added paramount & rakuten

---
+++ services/paramountplus.py
@@ -0,0 +1,14 @@
+import urllib.parse
+
+def get_headers():
+    headers = {}
+    return headers
+
+def get_params():
+    params = {}
+    encoded_params = urllib.parse.urlencode(params)
+    return encoded_params
+
+def get_data():
+    data = ""
+    return data

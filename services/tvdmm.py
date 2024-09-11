2c7c5c839697ac9845bdb3cca1990456abe189d5
unknown <apid9755@gmail.com>
2024-09-11 13:30:19 +0800

    added tvdmm & prime

--- services/tvdmm.py
+++ services/tvdmm.py
@@ -1,41 +1,9 @@
 def get_cookies():
-    cookies = {
-    'i3_ab': '10562ff6-dab5-4e96-9cc8-93e256f64c18',
-    'rieSh3Ee_ga': 'GA1.1.718738852.1724239853',
-    '_gcl_au': '1.1.602897855.1724239854',
-    'FPID': 'FPID2.2.OXfdGHolop%2B4SuPf8%2BfDOfE4in5IZRfUorfF53LrWRU%3D.1724239853',
-    'FPLC': 'Wx4uxugZfz2E5VY2pCwFvxLvYdSlwc67D6cJtTBlJx1IEGNf8UvdRXQZMFePPgjSbblWM7ZZ3dC3ssrG5I1hXHdsTlrgmn922IrTdqXm0t5StDgLMX69437BTT8zMQ%3D%3D',
-    'FPAU': '1.2.1255490027.1724239857',
-    '_yjsu_yjad': '1724239855.9fd083f1-780c-4921-ad5b-d71fced23903',
-    '_fbp': 'fb.1.1724239861858.709351377418546811',
-    '_tt_enable_cookie': '1',
-    '_ttp': 'r_ELUVyF3ix9jvWIvERAPp04KCo',
-    'FPGSID': '1.1724288329.1724288329.G-KQYE0DE5JW.Tc3jamnBRv5X-2o95p43vA',
-    '_uetsid': 'd6d9e0f05fb011ef9fd11b46e4269117',
-    '_uetvid': 'd6da20e05fb011efbc132733aa490619',
-    '_clck': 'xtvgvh%7C2%7Cfoj%7C0%7C1694',
-    '_clsk': '1ay0euu%7C1724288329813%7C1%7C0%7Cu.clarity.ms%2Fcollect',
-    'rieSh3Ee_ga_KQYE0DE5JW': 'GS1.1.1724288326.2.1.1724288352.0.0.1337901877',
-}
+    cookies = {}
     return cookies
 
 def get_headers():
-    headers = {
-    'accept': '*/*',
-    'accept-language': 'en-US,en;q=0.9,ms;q=0.8',
-    # 'cookie': 'i3_ab=10562ff6-dab5-4e96-9cc8-93e256f64c18; rieSh3Ee_ga=GA1.1.718738852.1724239853; _gcl_au=1.1.602897855.1724239854; FPID=FPID2.2.OXfdGHolop%2B4SuPf8%2BfDOfE4in5IZRfUorfF53LrWRU%3D.1724239853; FPLC=Wx4uxugZfz2E5VY2pCwFvxLvYdSlwc67D6cJtTBlJx1IEGNf8UvdRXQZMFePPgjSbblWM7ZZ3dC3ssrG5I1hXHdsTlrgmn922IrTdqXm0t5StDgLMX69437BTT8zMQ%3D%3D; FPAU=1.2.1255490027.1724239857; _yjsu_yjad=1724239855.9fd083f1-780c-4921-ad5b-d71fced23903; _fbp=fb.1.1724239861858.709351377418546811; _tt_enable_cookie=1; _ttp=r_ELUVyF3ix9jvWIvERAPp04KCo; FPGSID=1.1724288329.1724288329.G-KQYE0DE5JW.Tc3jamnBRv5X-2o95p43vA; _uetsid=d6d9e0f05fb011ef9fd11b46e4269117; _uetvid=d6da20e05fb011efbc132733aa490619; _clck=xtvgvh%7C2%7Cfoj%7C0%7C1694; _clsk=1ay0euu%7C1724288329813%7C1%7C0%7Cu.clarity.ms%2Fcollect; rieSh3Ee_ga_KQYE0DE5JW=GS1.1.1724288326.2.1.1724288352.0.0.1337901877',
-    'dnt': '1',
-    'origin': 'https://tv.dmm.com',
-    'priority': 'u=1, i',
-    'referer': 'https://tv.dmm.com/',
-    'sec-ch-ua': '"Not)A;Brand";v="99", "Google Chrome";v="127", "Chromium";v="127"',
-    'sec-ch-ua-mobile': '?0',
-    'sec-ch-ua-platform': '"Windows"',
-    'sec-fetch-dest': 'empty',
-    'sec-fetch-mode': 'cors',
-    'sec-fetch-site': 'same-site',
-    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/127.0.0.0 Safari/537.36',
-}
+    headers = {}
     return headers
 
 def get_data():
